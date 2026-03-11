8239d419c99ac52eeec3403fdf3fccf53263122e
VitaliyHatala <161298762+VitaliyHatala@users.noreply.github.com>
2026-03-11 18:46:27 +0100

    Update Random_Forest.py

--- Task 1/Random_Forest.py
+++ Task 1/Random_Forest.py
@@ -1,21 +1,16 @@
 from sklearn.ensemble import RandomForestClassifier
-
 from main_class import MnistClassifierInterface
 
-
 class RandomForestMnistClassifier(MnistClassifierInterface):
-
+    
     def __init__(self):
         self.model = RandomForestClassifier(n_estimators=100)
 
     def train(self, X_train, y_train):
         X_train = X_train.reshape(X_train.shape[0], -1)
-
         self.model.fit(X_train, y_train)
 
     def predict(self, X):
         X = X.reshape(X.shape[0], -1)
-
         prediction = self.model.predict(X)
-
         return prediction
