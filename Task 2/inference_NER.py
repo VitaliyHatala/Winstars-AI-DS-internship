a4e9f0033c86306371caa4ed60d6dec76b6134b3
VitaliyHatala <161298762+VitaliyHatala@users.noreply.github.com>
2026-03-11 23:23:23 +0100

    Update inference_NER.py

--- Task 2/inference_NER.py
+++ Task 2/inference_NER.py
@@ -1,25 +1,15 @@
 import torch
-
 from transformers import AutoTokenizer, AutoModelForTokenClassification
 
-# шлях до натренованої моделі
 
 model_path = "D:\\STUDY\\PyCharm\\PyCharm 2025.3.1.1\\Projects\\PythonProject3\\animal_ner_model"
 
-# завантажуємо модель і токенайзер
-
 tokenizer = AutoTokenizer.from_pretrained(model_path)
-
 model = AutoModelForTokenClassification.from_pretrained(model_path)
-
-# список міток
 
 label_list = ["O", "ANIMAL"]
 
-
 def predict_animals(text):
-    # токенізація
-
     tokens = text.split()
     inputs = tokenizer(
         tokens,
@@ -32,48 +22,30 @@ def predict_animals(text):
     # inference
 
     with torch.no_grad():
-
         outputs = model(**inputs)
-
     logits = outputs.logits
-
     predictions = torch.argmax(logits, dim=2)
-
     predicted_labels = [label_list[p] for p in predictions[0].tolist()]
-
     word_ids = inputs.word_ids()
-
     animals = []
-
     previous_word = None
-
+    
     for idx, word_id in enumerate(word_ids):
-
         if word_id is None:
             continue
-
         if word_id != previous_word:
-
             label = predicted_labels[idx]
-
             if label == "ANIMAL":
                 animals.append(tokens[word_id])
-
         previous_word = word_id
-
     return animals
 
 
 if __name__ == "__main__":
-
     text = input("Enter text: ")
-
     animals = predict_animals(text)
-
+    
     if animals:
-
         print("Animals found:", animals)
-
     else:
-
         print("No animals found.")
