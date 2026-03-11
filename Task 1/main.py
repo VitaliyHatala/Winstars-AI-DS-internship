89c2e34810aba43c99bd3228688b3f59d5e0bdaa
VitaliyHatala <161298762+VitaliyHatala@users.noreply.github.com>
2026-03-11 18:49:26 +0100

    Update main.py

--- Task 1/main.py
+++ Task 1/main.py
@@ -6,23 +6,17 @@ from Random_Forest import RandomForestMnistClassifier
 class MnistClassifier:
 
     def __init__(self, algorithm):
-
         if algorithm == "cnn":
             self.model = CNNMnistClassifier()
-
         elif algorithm == "nn":
             self.model = NeuralNetworkMnistClassifier()
-
         elif algorithm == "rf":
             self.model = RandomForestMnistClassifier()
-
         else:
             raise ValueError("Algorithm must be 'cnn', 'nn', or 'rf'")
 
     def train(self, X_train, y_train):
-
         self.model.train(X_train, y_train)
 
     def predict(self, X):
-
         return self.model.predict(X)
